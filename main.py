191ba4f0453c6adb55917dd3851e0163f287241a
Mohammad Jafari <m.jafari9877@gmail.com>
2021-05-12 20:37:04 +0430

    add timeit and utils file

--- main.py
+++ main.py
@@ -1,145 +1,213 @@
+from collections import namedtuple
+from itertools import chain, combinations
+
+import numpy as np
 import pandas as pd
-import time
-from functools import wraps
 
-start_all = time.time()
-
-# store each itemset in a list
-df = pd.read_csv('groceries.csv', header=None).fillna('')
-dataSet = df.values
+from utils import TransactionManager, timeit
 
 
-def timeit(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        print(f'{time.time() - start}s to do {func.__name__}')
-        return result
-
-    return wrapper
+def remove_nan_objects(dataframe):
+    return np.array(list(map(lambda x: x[~pd.isnull(x)], dataframe.values)), dtype=object)
 
 
-# create Candidate 1
+SupportRecord = namedtuple(
+    'SupportRecord', ('items', 'support'))
+RelationRecord = namedtuple(
+    'RelationRecord', SupportRecord._fields + ('ordered_statistics',))
+OrderedStatistic = namedtuple(
+    'OrderedStatistic', ('items_base', 'items_add', 'confidence', 'lift',))
+
+
+def create_next_candidates(prev_candidates, length):
+    """
+    Returns the apriori candidates as a list.
+    Arguments:
+        prev_candidates -- Previous candidates as a list.
+        length -- The lengths of the next candidates.
+    """
+    # Solve the items.
+    items = sorted(frozenset(chain.from_iterable(prev_candidates)))
+
+    # Create the temporary candidates. These will be filtered below.
+    tmp_next_candidates = (frozenset(x) for x in combinations(items, length))
+
+    # Return all the candidates if the length of the next candidates is 2
+    # because their subsets are the same as items.
+    if length < 3:
+        return list(tmp_next_candidates)
+
+    # Filter candidates that all of their subsets are
+    # in the previous candidates.
+    next_candidates = [
+        candidate for candidate in tmp_next_candidates
+        if all(
+            frozenset(x) in prev_candidates
+            for x in combinations(candidate, length - 1))
+    ]
+    return next_candidates
+
+
+def gen_support_records(transaction_manager, min_support, **kwargs):
+    """
+    Returns a generator of support records with given transactions.
+    Arguments:
+        transaction_manager -- Transactions as a TransactionManager instance.
+        min_support -- A minimum support (float).
+    Keyword arguments:
+        max_length -- The maximum length of relations (integer).
+    """
+    # Parse arguments.
+    max_length = kwargs.get('max_length')
+
+    # For testing.
+    _create_next_candidates = kwargs.get(
+        '_create_next_candidates', create_next_candidates)
+
+    # Process.
+    candidates = transaction_manager.initial_candidates()
+    length = 1
+    while candidates:
+        relations = set()
+        for relation_candidate in candidates:
+            support = transaction_manager.calc_support(relation_candidate)
+            if support < min_support:
+                continue
+            candidate_set = frozenset(relation_candidate)
+            relations.add(candidate_set)
+            yield SupportRecord(candidate_set, support)
+        length += 1
+        if max_length and length > max_length:
+            break
+        candidates = _create_next_candidates(relations, length)
+
+
+def gen_ordered_statistics(transaction_manager, record):
+    """
+    Returns a generator of ordered statistics as OrderedStatistic instances.
+    Arguments:
+        transaction_manager -- Transactions as a TransactionManager instance.
+        record -- A support record as a SupportRecord instance.
+    """
+    items = record.items
+    sorted_items = sorted(items)
+    for base_length in range(len(items)):
+        for combination_set in combinations(sorted_items, base_length):
+            items_base = frozenset(combination_set)
+            items_add = frozenset(items.difference(items_base))
+            confidence = (
+                    record.support / transaction_manager.calc_support(items_base))
+            lift = confidence / transaction_manager.calc_support(items_add)
+            yield OrderedStatistic(
+                frozenset(items_base), frozenset(items_add), confidence, lift)
+
+
+def filter_ordered_statistics(ordered_statistics, **kwargs):
+    """
+    Filter OrderedStatistic objects.
+    Arguments:
+        ordered_statistics -- A OrderedStatistic iterable object.
+    Keyword arguments:
+        min_confidence -- The minimum confidence of relations (float).
+        min_lift -- The minimum lift of relations (float).
+    """
+    min_confidence = kwargs.get('min_confidence', 0.0)
+    min_lift = kwargs.get('min_lift', 0.0)
+
+    for ordered_statistic in ordered_statistics:
+        if ordered_statistic.confidence < min_confidence:
+            continue
+        if ordered_statistic.lift < min_lift:
+            continue
+        yield ordered_statistic
+
+
+def apriori(transactions, **kwargs):
+    """
+    Executes Apriori algorithm and returns a RelationRecord generator.
+    Arguments:
+        transactions -- A transaction iterable object
+                        (eg. [['A', 'B'], ['B', 'C']]).
+    Keyword arguments:
+        min_support -- The minimum support of relations (float).
+        min_confidence -- The minimum confidence of relations (float).
+        min_lift -- The minimum lift of relations (float).
+        max_length -- The maximum length of the relation (integer).
+    """
+    # Parse the arguments.
+    min_support = kwargs.get('min_support', 0.1)
+    min_confidence = kwargs.get('min_confidence', 0.0)
+    min_lift = kwargs.get('min_lift', 0.0)
+    max_length = kwargs.get('max_length', None)
+
+    # Check arguments.
+    if min_support <= 0:
+        raise ValueError('minimum support must be > 0')
+
+    # Calculate supports.
+    transaction_manager = TransactionManager.create(transactions)
+    support_records = gen_support_records(
+        transaction_manager, min_support, max_length=max_length)
+
+    # Calculate ordered stats.
+    for support_record in support_records:
+        ordered_statistics = list(
+            filter_ordered_statistics(
+                gen_ordered_statistics(transaction_manager, support_record),
+                min_confidence=min_confidence,
+                min_lift=min_lift,
+            )
+        )
+        if not ordered_statistics:
+            continue
+        yield RelationRecord(
+            support_record.items, support_record.support, ordered_statistics)
+
+
+def print_rules(rules):
+    for item in rules:
+        # first index of the inner list
+        # Contains base item and add item
+        pair = item[0]
+        items = [x for x in pair]
+        print("Rule: " + items[0] + " -> " + items[1])
+
+        # second index of the inner list
+        print("Support: " + str(item[1]))
+
+        # third index of the list located at 0th
+        # of the third index of the inner list
+
+        print("Confidence: " + str(item[2][0][2]))
+        print("Lift: " + str(item[2][0][3]))
+        print("=====================================")
+
+
+def sort_rules(rules, *, by='lift'):
+    if by in ['lift', 'confidence']:
+        return sorted(
+            rules,
+            key=lambda x: getattr(x.ordered_statistics[0], by),
+            reverse=True
+        )
+    elif by == 'confidence':
+        return sorted(
+            rules,
+            key=lambda x: x.support,
+            reverse=True
+        )
+    else:
+        raise Exception('invalid value for "by" argument')
+
+
 @timeit
-def create_candidate1(data_set):
-    C1 = []
-    for transaction in data_set:
-        for item in transaction:
-            if not [item] in C1:
-                C1.append([item])
-
-    C1.sort()
-    return list(map(frozenset, C1))
+def run():
+    df = pd.read_csv('groceries.csv', header=None)
+    data_set = remove_nan_objects(df)
+    association_rules = apriori(data_set, min_support=0.005, min_confidence=0.2, min_lift=3)
+    rules_sorted_by_lift = sort_rules(association_rules, by='lift')
+    print_rules(rules_sorted_by_lift)
 
 
-# use frozen set so we can use it as a key in a dict
-@timeit
-def scanD(D, Ck, minSupport):
-    ssCnt = {}
-    for tid in D:
-        for can in Ck:
-            if can.issubset(tid):
-                if not can in ssCnt:
-                    ssCnt[can] = 1
-                else:
-                    ssCnt[can] += 1
-    numItems = float(len(D))
-    retList = []
-    supportData = {}
-    for key in ssCnt:
-        support = ssCnt[key] / numItems
-        if support >= minSupport:
-            retList.insert(0, key)
-        supportData[key] = support
-    return retList, supportData
-
-
-C1 = create_candidate1(dataSet)
-
-# D is a dataset in the setform.
-D = list(map(set, dataSet))
-
-# return datasets having support>min-support
-L1, suppDat0 = scanD(D, C1, 0.005)
-
-
-# creates candidate datasets
-# creates Ck
-@timeit
-def aprioriGen(Lk, k):
-    retList = []
-    lenLk = len(Lk)
-    for i in range(lenLk):
-        for j in range(i + 1, lenLk):
-            L1 = list(Lk[i])[:k - 2]
-            L2 = list(Lk[j])[:k - 2]
-            L1.sort()
-            L2.sort()
-            # if first k-2 elements are equal, set union
-            if L1 == L2:
-                retList.append(Lk[i] | Lk[j])
-    return retList
-
-
-@timeit
-def apriori(dataSet, minSupport=0.005):
-    C1 = create_candidate1(dataSet)
-    D = list(map(set, dataSet))
-    L1, supportData = scanD(D, C1, minSupport)
-    L = [L1]
-    k = 2
-    while (len(L[k - 2]) > 0):
-        Ck = aprioriGen(L[k - 2], k)
-        Lk, supK = scanD(D, Ck, minSupport)  # scan DB to get Lk
-        supportData.update(supK)
-        L.append(Lk)
-        k += 1
-    return L, supportData
-
-
-L, suppData = apriori(dataSet)
-aprioriGen(L[0], 2)
-
-
-@timeit
-def calcConf(freqSet, H, supportData, brl, minConf=0.2):
-    # create new list to return
-    prunedH = []
-    for conseq in H:
-        # calc confidence
-        conf = supportData[freqSet] / supportData[freqSet - conseq]
-        if conf >= minConf:
-            #print(freqSet - conseq, '-->', conseq, 'conf:', conf)
-            brl.append((freqSet - conseq, conseq, conf))
-            prunedH.append(conseq)
-    return prunedH
-
-
-@timeit
-def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.2):
-    m = len(H[0])
-    if (len(freqSet) > (m + 1)):  # try further merging
-        Hmp1 = aprioriGen(H, m + 1)  # create Hm+1 new candidates
-        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
-        if (len(Hmp1) > 1):  # need at least two sets to merge
-            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
-
-
-@timeit
-def generateRules(L, supportData, minConf=0.2):  # supportData is a dict coming from scanD
-    bigRuleList = []
-    for i in range(1, len(L)):  # only get the sets with two or more items
-        for freqSet in L[i]:
-            H1 = [frozenset([item]) for item in freqSet]
-            if (i > 1):
-                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
-            else:
-                calcConf(freqSet, H1, supportData, bigRuleList, minConf)
-    return bigRuleList
-
-
-L, suppData = apriori(dataSet, minSupport=0.005)
-rules = generateRules(L, suppData, minConf=0.2)
-
-print(f'{time.time() - start_all}s to do all things!')
+if __name__ == '__main__':
+    run()
---
+++ utils.py
@@ -0,0 +1,108 @@
+import time
+from functools import wraps
+
+
+class TransactionManager:
+    """
+    Transaction managers.
+    """
+
+    def __init__(self, transactions):
+        """
+        Initialize.
+        Arguments:
+            transactions -- A transaction iterable object
+                            (eg. [['A', 'B'], ['B', 'C']]).
+        """
+        self.__num_transaction = 0
+        self.__items = []
+        self.__transaction_index_map = {}
+
+        for transaction in transactions:
+            self.add_transaction(transaction)
+
+    def add_transaction(self, transaction):
+        """
+        Add a transaction.
+        Arguments:
+            transaction -- A transaction as an iterable object (eg. ['A', 'B']).
+        """
+        for item in transaction:
+            if item not in self.__transaction_index_map:
+                self.__items.append(item)
+                self.__transaction_index_map[item] = set()
+            self.__transaction_index_map[item].add(self.__num_transaction)
+        self.__num_transaction += 1
+
+    def calc_support(self, items):
+        """
+        Returns a support for items.
+        Arguments:
+            items -- Items as an iterable object (eg. ['A', 'B']).
+        """
+        # Empty items is supported by all transactions.
+        if not items:
+            return 1.0
+
+        # Empty transactions supports no items.
+        if not self.num_transaction:
+            return 0.0
+
+        # Create the transaction index intersection.
+        sum_indexes = None
+        for item in items:
+            indexes = self.__transaction_index_map.get(item)
+            if indexes is None:
+                # No support for any set that contains a not existing item.
+                return 0.0
+
+            if sum_indexes is None:
+                # Assign the indexes on the first time.
+                sum_indexes = indexes
+            else:
+                # Calculate the intersection on not the first time.
+                sum_indexes = sum_indexes.intersection(indexes)
+
+        # Calculate and return the support.
+        return float(len(sum_indexes)) / self.__num_transaction
+
+    def initial_candidates(self):
+        """
+        Returns the initial candidates.
+        """
+        return [frozenset([item]) for item in self.items]
+
+    @property
+    def num_transaction(self):
+        """
+        Returns the number of transactions.
+        """
+        return self.__num_transaction
+
+    @property
+    def items(self):
+        """
+        Returns the item list that the transaction is consisted of.
+        """
+        return sorted(self.__items)
+
+    @staticmethod
+    def create(transactions):
+        """
+        Create the TransactionManager with a transaction instance.
+        If the given instance is a TransactionManager, this returns itself.
+        """
+        if isinstance(transactions, TransactionManager):
+            return transactions
+        return TransactionManager(transactions)
+
+
+def timeit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f'{time.time() - start}s to {func.__name__}')
+        return result
+
+    return wrapper
